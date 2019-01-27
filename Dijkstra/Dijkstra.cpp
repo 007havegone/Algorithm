f872ab1c96ccf03fe00fd6288845da3f2e4dfa0d
007havegone <996209949@qq.com>
2019-01-27 15:31:51 +0800

    realize the Dijkstra

---
+++ Dijkstra/Dijkstra.cpp
@@ -0,0 +1,73 @@
+#include <iostream>
+#include <queue>
+#include <algorithm>
+#include <cstring>
+using namespace std;
+//下标从1开始 
+const int INF = 10e7;//无穷大
+int n, m, mp[107][107];//城市，线路，地图 
+bool flag[107];//是否访问
+int Dis[107];
+struct Node {
+	int u;//城市
+	int step;//距离
+	Node(int m_u, int m_step) :u(m_u), step(m_step) {
+	}
+	//注意要const，因为是成员函数 
+	bool operator<(const Node& n)const//最小值优先 
+	{
+		return step > n.step;
+	}
+};
+
+void disjktra(int sta)
+{
+	priority_queue<Node> que;
+	que.push(Node(sta, 0));
+	memset(flag, 0, sizeof(flag));
+	for (int i = 1; i <= n; i++)
+		Dis[i] = INF;
+	Dis[sta] = 0;
+	while (!que.empty()) {
+		Node top = que.top();
+		que.pop();
+		int t = top.u;
+		if (flag[t])
+			continue;
+		flag[t] = 1;
+		for (int i = 1; i <= n; i++)
+		{
+			if (!flag[i] && mp[t][i] < INF)
+			{
+				if (Dis[i] > Dis[t] + mp[t][i])
+					Dis[i] = Dis[t] + mp[t][i];
+				que.push(Node(i, Dis[i]));
+			}
+		}
+	}
+}
+int main()
+{
+	cout << "输入城市数和路线数" << endl;
+	int sta;
+	int ci, cj, dis;
+	while (cin >> n >> m)
+	{
+		for (int i = 1; i <= n; i++)
+			for (int j = 1; j <= n; j++)
+				mp[i][j] = INF;//初始化mp
+
+		cout << "输入路线" << endl;
+		while (m--)
+		{
+			cin >> ci >> cj >> dis;
+			mp[ci][cj] = min(mp[ci][cj], dis);
+		}
+		cout << "输入出发点" << endl;
+		cin >> sta;
+		disjktra(sta);
+		cout << "当前位置为 " << sta << endl;
+		for (int i = 1; i <= n; i++)
+			cout << "到位置 " << i << "的距离为 " << Dis[i] << endl;
+	}
+}
