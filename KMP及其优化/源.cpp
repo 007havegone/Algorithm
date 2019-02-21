d16a89ba431b3292a90e0c124656dc22053ba325
007havegone <996209949@qq.com>
2019-02-22 00:27:35 +0800

    添加KMP算法实现的描述

--- KMP及其优化/源.cpp
+++ KMP及其优化/源.cpp
@@ -1,3 +1,8 @@
+/*
+	本程序实现的KMP算法对串的存储位置从0开始
+	临界点即模式串j前面没有任何符合相同子串时，设next[j]=-1
+	输入输出返回的位序从1开始
+*/
 #include <iostream>
 #include <algorithm>
 #include <string>
@@ -5,7 +10,9 @@ using namespace std;
 const int N = 1000;
 int Next[N];
 int NextVal[N];
-int index_BF(const string& s,const string& t, int pos) {//pos开始查找点
+//最好 O（n+m） n为主串长度，m为模式串长度
+//最坏 O（n*m）
+int index_BF(const string& s,const string& t, int pos) {//pos开始查找点，位序从1开始
 	int i = pos - 1, j = 0;
 	int lens = s.length();
 	int lent = t.length();
@@ -19,22 +26,26 @@ int index_BF(const string& s,const string& t, int pos) {//pos开始查找点
 			j = 0;
 		}
 	}
-	if (j == lent)return i - lent + 1;
+	if (j == lent)return i - lent + 1;//返回的位序从1开始
 	return 0;
 }
+//时间复杂度 O（m+n）
+/*
+	设模式串为t，假设第j个发生不匹配的next[j]=k
+*/
 void getNext(string str) {
 	int i = 0;
-	int j = -1;
-	Next[0] = -1;
+	int j = -1;//保存第j个位置的next值
+	Next[0] = -1;//设置临界点next[0]=-1
 	int len = str.length();
 	while (i < len) {
-		if (j == -1 || str[i] == str[j]) {
+		if (j == -1 || str[i] == str[j]) {//直到临界点或者t[k]=t[j]则next[j+1]=next[j]+1
 			i++;
 			j++;
 			Next[i] = j;
 		}
 		else
-			j = Next[j];
+			j = Next[j];//否则相当于自身作为模式串和主串，查看t[next[k]]与t[j]的关系
 	}
 }
 //optimal
@@ -46,6 +57,9 @@ void getNextVal(string str) {
 	while (i < len) {
 		if (j == -1 || str[i] == str[j]) {
 			i++, j++;
+			/*如果当第j个位置发生不匹配，且有t[j]=t[k],因为如果当t[j]与主串s[i]不匹配，不必要再与t[k]即t[next[j]]项比较，而是
+			  与t[next[k]]相比较，即 next[j]= k'= next[k]
+			*/
 			if (str[i] != str[j]) {
 				NextVal[i] = j;
 			}else
@@ -71,6 +85,12 @@ int index_KMP(const string& str, const string& t, int pos) {
 		return i - lent + 1;
 	return 0;
 }
+/*
+	BF算法之所以仍在使用，虽然时间复杂度为O（m*n）,但实际使用中接近O（m+n）
+	KMP算法只有在主串和模式串之间存在很多部分匹配的情况下（则主串i的指针需要回溯），才会
+	显得比BF算法快得多。但是KMP算法最大的特点是指示主串的指针无须回溯，整个过程只需对主串从
+	头到尾扫描一遍，对处理外设输入庞大的文件很有效，可以边读便匹配
+*/
 int main() {
 	string s = "HelloHelloabaaccWorld";
 	string t = "abaacc";
